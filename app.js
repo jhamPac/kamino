c65c1c692da6b411b257dba21f1eb98997728f27
Cloud <cloud@kevia.me>
2018-01-14 14:29:19 -0700

    added an interval to ping twitter api

--- app.js
+++ app.js
@@ -10,21 +10,27 @@ const twitterClient = new Twitter({
 })
 
 let params = {
-  q: '#rosebowl',
-  count: 2,
+  q: '#fashion',
+  geocode: '33.5,-111.933333,5mi',
+  count: 10,
   result_type: 'recent',
   lang: 'en'
 }
 
-twitterClient.get('search/tweets', params)
-  .then(result => {
-    let { statuses } = result
+let twitterGet = () => {
+  twitterClient.get('search/tweets', params)
+    .then(result => {
+      let { statuses } = result
 
-    statuses.forEach((status) => {
-      let obj = { id: status.id_str }
-      twitterClient.post('favorites/create', obj)
-      .then(response => console.log(`The response is: ${response}`))
-      .catch(err => console.error(`Liking had an error: ${err}`))
+      statuses.forEach((status) => {
+        console.log(status.user)
+        // let obj = { id: status.id_str }
+        // twitterClient.post('favorites/create', obj)
+        // .then(response => console.log(`The response is: ${response}`))
+        // .catch(err => console.error(`Liking had an error: ${err}`))
+      })
     })
-  })
-  .catch(error => console.log(error))
+    .catch(error => console.log(error))
+}
+
+setInterval(twitterGet, 60000)
